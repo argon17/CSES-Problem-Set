4bc0f62284670f0d63c784ca4caf99e4d016859f
Prem <81183081+argon17@users.noreply.github.com>
2021-06-11 13:00:00 +0530

    Update 6NumberSpiral.cpp

--- 6NumberSpiral.cpp
+++ 6NumberSpiral.cpp
@@ -4,7 +4,6 @@ void solve()
     cin>>r>>c;
     ll mx = max(r, c);
     ll dg = mx*mx - mx + 1;
-    if( r>c ) ans = mx&1 ? dg - (r-c) : dg + (r-c);
-    else ans = mx&1 ? dg - (r-c) : dg + (r-c);
+    ans = mx&1 ? dg - (r-c) : dg + (r-c);
     cout<<ans<<endl;
 }
